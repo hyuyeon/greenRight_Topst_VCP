d62bb23ca761ed2525a0d57aa53b156dc82848a0
cyh <wer234ve@gmail.com>
2026-08-12 15:01:15 +0900

    temp

--- sources/app.sample/app.qos/temporal_qos.c
+++ sources/app.sample/app.qos/temporal_qos.c
@@ -78,12 +78,12 @@ void TemporalQos_CompensateLatency(
     compensatedCandi->y = (uint16_t)(predictedY + 0.5);
 }
 
-void TemporalQos_PrintSrcAndCurrentTime(uint16_t logId, )
-{
-    // [T1] vehicle=1 src_ts12=1234 current=07:30:15.123
+// void TemporalQos_PrintSrcAndCurrentTime(uint16_t logId, )
+// {
+//     // [T1] vehicle=1 src_ts12=1234 current=07:30:15.123
 
-    return (uint16_t)(
-        (currentTimestamp - sourceTimestamp) &
-        TEMPORAL_QOS_TIMESTAMP_MASK
-    );
-}
+//     return (uint16_t)(
+//         (currentTimestamp - sourceTimestamp) &
+//         TEMPORAL_QOS_TIMESTAMP_MASK
+//     );
+// }
